558346e0d988a01bc73ea665b6669655e216c832
Jesse Hon <honkalong@gmail.com>
2015-05-07 23:55:33 -0700

    Create basic viewset for threads. Add url paths for viewsets.

--- etsy_convos/convos/views.py
+++ etsy_convos/convos/views.py
@@ -6,3 +6,7 @@ from .serializers import *
 class ConvoViewSet(viewsets.ModelViewSet):
     queryset = Convo.objects.all()
     serializer_class = ConvoSerializer
+
+class ConvoThreadViewSet(viewsets.ModelViewSet):
+    queryset = ConvoThread.objects.all()
+    serializer_class = ConvoThreadSerializer
